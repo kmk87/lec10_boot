5aeab4352e8d5fcb04261dc3b23731581f5c5fdb
kmk87 <minkyoungk24@gmail.com>
2024-08-08 14:36:49 +0900

    [김민경] 목록 조회(페이징)

--- src/main/java/com/gn/spring/board/controller/BoardViewController.java
+++ src/main/java/com/gn/spring/board/controller/BoardViewController.java
@@ -1,11 +1,15 @@
 package com.gn.spring.board.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.gn.spring.board.domain.BoardDto;
@@ -24,9 +28,12 @@ public class BoardViewController {
 	}
 	
 	@GetMapping("/board")
-	public String selectBoardList() {
-		List<BoardDto> resultList = boardService.selectBoardList();
+	public String selectBoardList(Model model,
+			@PageableDefault(page=0, size = 5, sort="regDate", direction=Sort.Direction.DESC) 
+	Pageable pageable) {
+		Page<BoardDto> resultList = boardService.selectBoardList(pageable);
 		LOGGER.debug(resultList.toString());
+		model.addAttribute("resultList",resultList);
 		return "board/list";
 	}
 	
--- src/main/java/com/gn/spring/board/service/BoardService.java
+++ src/main/java/com/gn/spring/board/service/BoardService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.gn.spring.board.domain.Board;
@@ -22,18 +25,32 @@ public class BoardService {
 		this.boardRepository = boardRepository;
 	}
 	
-	public List<BoardDto> selectBoardList(){
+	public Page<BoardDto> selectBoardList(Pageable pageable){
 		
-		List<Board> boardList = boardRepository.findAll();
+		Page<Board> boardList = boardRepository.findAll(pageable);
 		
 		List<BoardDto> boardDtoList = new ArrayList<BoardDto>();
-		// Board(Entity)를 BoardDto로 바꿔주기
-		for(Board board : boardList) {
-			BoardDto boardDto = new BoardDto().toDto(board);
-			boardDtoList.add(boardDto);
+		for(Board b : boardList) {
+			BoardDto dto = new BoardDto().toDto(b);
+			boardDtoList.add(dto);
 		}
 		
-		return boardDtoList;
+		
+		return new PageImpl<>(boardDtoList,pageable,boardList.getTotalElements());
+		
+		
+		
+		
+//		List<Board> boardList = boardRepository.findAll();
+//		
+//		List<BoardDto> boardDtoList = new ArrayList<BoardDto>();
+//		// Board(Entity)를 BoardDto로 바꿔주기
+//		for(Board board : boardList) {
+//			BoardDto boardDto = new BoardDto().toDto(board);
+//			boardDtoList.add(boardDto);
+//		}
+//		
+//		return boardDtoList;
 		
 		
 		
